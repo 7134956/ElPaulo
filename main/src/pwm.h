fd3bd2122a81a4aee7607e43b0a118c2963a4de8
Andrey <7134956@gmail.com>
2015-12-11 14:01:36 +0200

    Trivial

--- main/src/pwm.h
+++ main/src/pwm.h
@@ -1,3 +1,6 @@
+#ifndef _PWM_H_
+#define _PWM_H_
+
 #include "u8g.h"
 
 #define PWM_MAX 255
@@ -6,3 +9,5 @@
 void PWM_init(void);
 void PWMSet(uint8_t, uint8_t);
 uint8_t PWMGet(uint8_t);
+
+#endif /* _PWM_H_ */

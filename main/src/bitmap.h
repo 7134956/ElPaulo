00d22019ce34bed621da4b3c71929f6dd3537d0e
7134956 <7134956@gmail.com>
2016-01-15 17:30:35 +0300

    Сделал шрифт разноширотным

--- main/src/bitmap.h
+++ main/src/bitmap.h
@@ -4,7 +4,7 @@
 
 extern const u8g_fntpgm_uint8_t u8g_font_elpauloIco[399];
 extern const u8g_fntpgm_uint8_t u8g_font_elpaulo16n[450];
-extern const u8g_fntpgm_uint8_t u8g_font_elpaulo20[3620];
+extern const u8g_fntpgm_uint8_t u8g_font_elpaulo20[3724];
 extern const u8g_fntpgm_uint8_t u8g_font_elpaulo32n[1390];
 extern const u8g_fntpgm_uint8_t u8g_font_ncenR10[3813];
 extern const u8g_fntpgm_uint8_t u8g_font_7x13B[2172];

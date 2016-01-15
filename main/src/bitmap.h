310a56eb70eb0fd4809add490b4f7bf139b6e42e
7134956 <7134956@gmail.com>
2016-01-15 18:32:16 +0300

    Правка шрифта

--- main/src/bitmap.h
+++ main/src/bitmap.h
@@ -2,17 +2,19 @@
 #define _BITMAP_H_
 #include "u8g.h"
 
-extern const u8g_fntpgm_uint8_t u8g_font_elpauloIco[399];
-extern const u8g_fntpgm_uint8_t u8g_font_elpaulo16n[450];
-extern const u8g_fntpgm_uint8_t u8g_font_elpaulo20[3724];
-extern const u8g_fntpgm_uint8_t u8g_font_elpaulo32n[1390];
-extern const u8g_fntpgm_uint8_t u8g_font_ncenR10[3813];
-extern const u8g_fntpgm_uint8_t u8g_font_7x13B[2172];
-extern const u8g_fntpgm_uint8_t u8g_font_helvR24[10931];
+/* Fonts */
+extern const u8g_fntpgm_uint8_t u8g_font_elpauloIco[];
+extern const u8g_fntpgm_uint8_t u8g_font_elpaulo16n[];
+extern const u8g_fntpgm_uint8_t u8g_font_elpaulo20[];
+extern const u8g_fntpgm_uint8_t u8g_font_elpaulo32n[];
+extern const u8g_fntpgm_uint8_t u8g_font_ncenR10[];
+extern const u8g_fntpgm_uint8_t u8g_font_7x13B[];
+extern const u8g_fntpgm_uint8_t u8g_font_helvR24[];
 
-extern const unsigned char u8g_logo_bits[];
-extern const unsigned char u8g_neighbor_bits[];
-extern const unsigned char u8g_farthest_bits[];
-extern const unsigned char u8g_lightdisplay_bits[];
+/* Bitmaps */
+extern const u8g_fntpgm_uint8_t u8g_logo_bits[];
+extern const u8g_fntpgm_uint8_t u8g_neighbor_bits[];
+extern const u8g_fntpgm_uint8_t u8g_farthest_bits[];
+extern const u8g_fntpgm_uint8_t u8g_lightdisplay_bits[];
 
 #endif /* _BITMAP_H_ */
